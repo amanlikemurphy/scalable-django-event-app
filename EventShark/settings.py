12e7563f02752362a9052570292f6725f2faef80
amanlikemurphy <olukoya.elo@gmail.com>
2023-04-04 19:00:15 +0100

    Changed URL

--- EventShark/settings.py
+++ EventShark/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-3l-_*^i!i441mydqxn=kz#(glftj@5nd-if+3f7hdbj+e+gqs9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'eventshark.eu-west-1.elasticbeanstalk.com', 'http://eventshark.eu-west-1.elasticbeanstalk.com']
 
 AUTH_USER_MODEL = 'base.User'
 
