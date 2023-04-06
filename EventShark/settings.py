e7d58e25d006367a454dd97c3cc4fc584e09693b
EC2 Default User <ec2-user@ip-172-31-0-244.ec2.internal>
2023-04-06 05:11:27 +0000

    deployed version

--- EventShark/settings.py
+++ EventShark/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-3l-_*^i!i441mydqxn=kz#(glftj@5nd-if+3f7hdbj+e+gqs9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['scalable-env.eba-funyx2mj.us-east-1.elasticbeanstalk.com']
 
 
 # Application definition']
