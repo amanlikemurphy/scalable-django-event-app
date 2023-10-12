3edca5f6b2308512c26b1ad00781361db21c3dee
Murphy Elo <olukoya.elo@gmail.com>
2023-10-12 20:11:19 +0100

    Add Env

--- EventShark/settings.py
+++ EventShark/settings.py
@@ -12,10 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 
-from dotenv import load_dotenv
 import os
-
-load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -30,7 +27,7 @@ SECRET_KEY = 'django-insecure-3l-_*^i!i441mydqxn=kz#(glftj@5nd-if+3f7hdbj+e+gqs9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['eventsharkapp.eba-htd6jbuz.us-west-2.elasticbeanstalk.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition']
@@ -94,23 +91,23 @@ WSGI_APPLICATION = 'EventShark.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-   'default': {
-       'ENGINE': 'django.db.backends.postgresql',
-       'NAME': os.environ.get ('name'),
-       'USER': os.environ.get ('user'),
-       'PASSWORD': os.environ.get ('password'),
-       'HOST': os.environ.get ('host'),
-        'PORT': 5432
-    }
-}
-
 #DATABASES = {
-    #'default': {
-        #'ENGINE': 'django.db.backends.sqlite3',
-        #'NAME': BASE_DIR / 'db.sqlite3',
+   #'default': {
+       #'ENGINE': 'django.db.backends.postgresql',
+       #'NAME': os.environ.get ('name'),
+       #'USER': os.environ.get ('user'),
+       #'PASSWORD': os.environ.get ('password'),
+       #'HOST': os.environ.get ('host'),
+        #'PORT': 5432
     #}
 #}
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 # Password validation
